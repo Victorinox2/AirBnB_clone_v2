4ab7f5a59d02dca80cc185313088fb4192a9dd6f
Victorinox2 <uwaomaviktor@yahoo.com>
2023-12-16 18:26:32 -0800

    updated

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""This module defines a class to manage file storage for hbnb clone"""
+"""This module defines a class to manage the file storage for hbnb clone"""
 import json
 
 
@@ -8,9 +8,18 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+        filtered_objs = {}
+
+        if cls:
+            for key, obj in self.__objects.items():
+                if isinstance(obj, cls):
+                    filtered_objs[key] = obj
+
+            return filtered_objs
+        else:
+            return FileStorage.__objects
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
@@ -21,9 +30,20 @@ class FileStorage:
         with open(FileStorage.__file_path, 'w') as f:
             temp = {}
             temp.update(FileStorage.__objects)
+
             for key, val in temp.items():
                 temp[key] = val.to_dict()
             json.dump(temp, f)
+
+    def delete(self, obj=None):
+        """ Delete obj from storage"""
+
+        if obj:
+            for key in self.__objects.keys():
+                if obj == self.__objects[key]:
+                    self.__objects.pop(key)
+
+                    break
 
     def reload(self):
         """Loads storage dictionary from file"""
@@ -44,7 +64,8 @@ class FileStorage:
             temp = {}
             with open(FileStorage.__file_path, 'r') as f:
                 temp = json.load(f)
+
                 for key, val in temp.items():
-                        self.all()[key] = classes[val['__class__']](**val)
+                    self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
